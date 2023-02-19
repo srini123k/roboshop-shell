604f386cbfb4e282d215481a96ac1f9653ce37bb
Srini K <srinikdevops0131@gmail.com>
2023-02-18 21:22:51 -0800

    Roboshop components

---
+++ mongodb.sh
@@ -0,0 +1,7 @@
+cp config.mongodb.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+
+#Update config file vim /etc/mongod.conf from 127.0.0.1 with 0.0.0.0
+
