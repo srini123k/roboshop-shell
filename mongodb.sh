bc232c187c31a90b98bb877b5058a52e00395cff
Srini K <srinikdevops0131@gmail.com>
2023-02-20 15:27:21 -0800

    updating ./roboshop-shell

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,18 @@
+source common.sh
+
+print_head "Setup MongoDB repository"
+
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head "Install Mongodb service"
 yum install mongodb-org -y
+
+print_head "Enable Mongodb"
 systemctl enable mongod
+
+print_head "Start Mongodb"
 systemctl start mongod
 
 #Update config file vim /etc/mongod.conf from 127.0.0.1 with 0.0.0.0
-
+print_head "restart MongoDB"
 systemctl restart mongod
