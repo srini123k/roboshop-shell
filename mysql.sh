8a78b5c0f8a38e81f809170b8f0a1170a3418540
Srini K <srinikdevops0131@gmail.com>
2023-02-23 11:57:12 -0800

    updating ./roboshop-shell

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 mysql_root_password =$1
-if [ -z "${mysql_root_password}"];then
+if [ -z "${mysql_root_password}" ];then
   echo -e "\e[31mMissing mysql root password argument\e[0m"
   exit 1
 fi
